4c404fd892b40f06079cd85e27b3fa2f69d696b1
Keythe18 <alexis.lansucki@gmail.com>
2020-11-30 11:04:18 +0100

    first commit

---
+++ useStrict.js
@@ -0,0 +1,12 @@
+console.log('Start');
+'use strict';
+
+/* use strict est une nouvelle fonctionnalite de ES5
+   qui permet de placer un programme ou une fonction dans un contexte strict.
+   Un contexte strict empeche certaines actions d'etre effectuee et prevoit
+   d'avantage d'exceptions.
+*/
+
+var maVariable = 'Un script en mode strict!';
+console.log(maVariable);
+
